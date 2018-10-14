9a6fc14e6989bcd4b1c9d7186d41c9fadf2c5e03
Berry <bsemexan@gmail.com>
2018-10-13 21:14:15 -0400

    merge_sort

---
+++ merge_sort.rb
@@ -0,0 +1,21 @@
+#https://en.wikibooks.org/wiki/Algorithm_Implementation/Sorting/Merge_sort#Ruby
+
+def mergesort(list)
+    return list if list.size <= 1
+    mid   = list.size / 2
+    left  = list[0...mid]
+    right = list[mid...list.size]
+    merge(mergesort(left), mergesort(right))
+  end
+  
+  def merge(left, right)
+    sorted = []
+    until left.empty? || right.empty?
+      if left.first <= right.first
+        sorted << left.shift
+      else
+        sorted << right.shift
+      end
+    end
+    sorted.concat(left).concat(right)
+  end
